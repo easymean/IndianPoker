8f65c21e493d7740cfd9795b614fb789a7920499
easymean <m2162003@gmail.com>
2020-10-19 16:42:36 +0900

    <feat> add room choice page

--- game/urls.py
+++ game/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import CreateUser, CreateRoom, enter_room, index
+from .views import CreateUser, CreateRoom, enter_room, index, select_room
 
 app_name = "game"
 
 urlpatterns = [
     path("", index, name='index'),
     path("user/", CreateUser.as_view()),
+    path("choice/", select_room, name='select_room'),
     path("room/<str:room_id>/", enter_room, name='enter_room'),
     path("room/", CreateRoom.as_view()),
 ]
--- game/views.py
+++ game/views.py
@@ -4,14 +4,14 @@ from rest_framework.decorators import api_view
 from rest_framework import serializers, status, generics
 from rest_framework.response import Response
 
-from .models import user_enter_room
+from .models import enter_room as user_enter_room
 from utils.redis_client import r
 
 from .serializers import UserSerializer, RoomSerializer
 
-
 def index(request):
     return render(request, 'game/index.html', {})
+
 
 class CreateUser(generics.CreateAPIView):
     serializer_class = UserSerializer
@@ -31,17 +31,20 @@ class CreateRoom(generics.CreateAPIView):
         response = super(CreateRoom, self).create(request, *args, **kwargs)
         data = response.data
         room_id = data["id"]
-        return redirect(room_id + "/")
+        user_id = request.COOKIES.get('user_id')
+        user_enter_room(user_id=user_id, room_id=room_id)
+        return response
 
+
+@api_view(['GET'])
+def select_room(request):
+    return render(request, 'game/room_info.html', {})
 
 
 @api_view(['GET'])
 def enter_room(request, room_id):
-    user_id = request.COOKIES['user_id']
-    user_enter_room(user_id=user_id, room_id=room_id)
     return render(request, 'game/room.html', {
         'room_id': room_id,
-        'user_id': user_id
     })
 
 
