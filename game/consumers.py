78b1510ca5518881dc77c0ea65548174e001f2c1
easymean <m2162003@gmail.com>
2020-12-23 22:59:26 +0900

    <fix> fix server-cli message format

--- game/consumers.py
+++ game/consumers.py
@@ -1,10 +1,9 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-
 # I/O가 왼료된 경우에만 await 함수부분이 실행됨
-from game.models import EnterMessage, ExitMessage, get_nickname, exit_room, ReadyMessage, get_ready, cancel_ready, \
-    WaitMessage
+from game.models import get_nickname, exit_room, get_ready, cancel_ready, \
+    start_game, are_both_users_ready, GameMessage
 
 
 class GameInfoConsumer(AsyncWebsocketConsumer):
@@ -31,70 +30,104 @@ class GameInfoConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message_type = text_data_json['type']
         sender_id = text_data_json['sender_id']
+        bet = text_data_json['bet']
         nickname = get_nickname(sender_id)
 
+        # 타입에 따라 메세지 파싱
         lower_message_type = message_type.lower()
         str_list = [lower_message_type, "message"]
-
         parsed_type = '_'.join(str_list)
 
         # send message to room group
         await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    'type': parsed_type,
-                    'sender_id': sender_id,
-                    'nickname': nickname
-                }
+            self.room_group_name,
+            {
+                'type': parsed_type,
+                'sender_id': sender_id,
+                'nickname': nickname,
+                'bet': bet,
+            }
         )
 
-    async def send_message(self, message, cli_msg_type):
+    async def send_message(self, message, type):
         await self.send(
             text_data=json.dumps({
                 'message': message,
-                'type': cli_msg_type
+                'type': type
             })
         )
 
     # receive message from room group
     async def enter_message(self, event):
-        sender_id = event['sender_id']
         nickname = event['nickname']
 
-        info_message = EnterMessage(room_id=self.room_name, sender_id=sender_id, nickname=nickname)
-
+        msg_obj = GameMessage()
+        msg_obj.enter_message(nickname=nickname)
         # send message to websocket
-        await self.send_message(info_message, 'room')
+        await self.send_message(msg_obj.to_json(), msg_obj.type)
 
     async def exit_message(self, event):
         sender_id = event['sender_id']
         nickname = event['nickname']
 
-        info_message = ExitMessage(room_id=self.room_name, sender_id=sender_id, nickname=nickname)
+        msg_obj = GameMessage()
+        msg_obj.exit_message(nickname)
 
         # delete user from database
         exit_room(self.room_name, sender_id)
 
         # send message to websocket
-        await self.send_message(info_message, 'room')
+        await self.send_message(msg_obj.to_json(), msg_obj.type)
 
     async def ready_message(self, event):
         sender_id = event['sender_id']
         nickname = event['nickname']
 
-        info_message = ReadyMessage(room_id=self.room_name, sender_id=sender_id, nickname=nickname)
-
+        msg_obj = GameMessage()
+        msg_obj.ready_message(nickname=nickname)
         get_ready(user_id=sender_id)
+        await self.send_message(msg_obj.to_json(), msg_obj.type)
 
-        await self.send_message(info_message, 'game')
+        if are_both_users_ready(self.room_name):
+            msg_obj.start_message(nickname)
+            await self.send_message(msg_obj.to_json(), msg_obj.type)
+
+    async def start_message(self, event):
+        sender_id = event['sender_id']
+        nickname = event['nickname']
+        start_game(self.room_name)
+
+        msg_obj = GameMessage()
+        msg_obj.start_game(room_id=self.room_name, me=sender_id, my_nickname=nickname)
+        await self.send_message(msg_obj.to_json(), msg_obj.type)
 
     async def wait_message(self, event):
         sender_id = event['sender_id']
         nickname = event['nickname']
 
-        info_message = WaitMessage(room_id=self.room_name, sender_id=sender_id, nickname=nickname)
-
+        msg_obj = GameMessage()
+        msg_obj.wait_message(nickname)
         cancel_ready(user_id=sender_id)
 
-        await self.send_message(info_message, 'game')
+        await self.send_message(msg_obj.to_json(), msg_obj.type)
 
+    async def check_message(self, event):
+        sender_id = event['sender_id']
+        nickname = event['nickname']
+        # bet = event['bet']
+        pass
+
+    async def bet_message(self, event):
+        sender_id = event['sender_id']
+        nickname = event['nickname']
+        # bet = event['bet']
+        pass
+
+    async def call_message(self, event):
+        pass
+
+    async def raise_message(self, event):
+        pass
+
+    async def die_message(self, event):
+        pass
