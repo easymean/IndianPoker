92f116e9d78a2bd444a1805c492ad98895e0fcad
easymean <m2162003@gmail.com>
2020-12-30 14:05:23 +0900

    <feat> add betting methods

--- game/apis/betting.py
+++ game/apis/betting.py
@@ -1,30 +1,55 @@
+from game.apis.common import parse_bytes_into_str
 from utils.redis_client import r
+
+
+def if_round_ended(room_id):
+    if r.hexists(room_id, 'last_choice'):
+        return True
+    else:
+        r.hset(room_id, 'last_choice', 'CHECK')
+        return False
 
 
 def check_betting(room_id, user_id, bet):
     z_name = f'{room_id}:betting'
-    r.zincrby(z_name, bet, user_id)
-
-    last_choice = r.hget(room_id, 'last_choice').decode()
-
-    if last_choice == 'CHECK':
-        return True
-    else:
-        r.hset(room_id, 'last_choice', 'CHECK')
-        r.hincrby(room_id, 'order', 1)
-        return False
+    increment = r.zincrby(z_name, bet, user_id)
+    r.hset(user_id, 'point', -bet)
+    return increment
 
 
-def call_betting(room_id, user_id):
-    pass
-
-
-def raise_betting(room_id, user_id, increment):
+def call_betting(room_id, user_id, bet):
     z_name = f'{room_id}:betting'
-    increased = r.zincrby(z_name, {user_id: increment})
-    r.hincrby(room_id, 'order', 1)
+    r.hset(user_id, 'point', -bet)
+    r.hset(room_id, 'last_choice', 'CALL')
+    increment = r.zincrby(z_name, bet, user_id)
+    return increment
+
+
+def raise_betting(room_id, user_id, bet):
+    z_name = f'{room_id}:betting'
+    r.hset(user_id, 'point', -bet)
+    increased = r.zincrby(z_name, bet, user_id)
+
+    if r.hexists(room_id, 'last_choice'):
+        r.hset(room_id, 'last_choice', 'RAISE')
+    else:
+        r.hset(room_id, 'last_choice', 'BET')
     return increased
 
 
-def die(room_id, user_id, this_round):
-    pass
+# 포기한 플레이어 카드가 10이면 5포인트를 잃는다.
+def loose_when_die(winner, loser):
+    r.hincrby(winner, 'point', 10)
+    r.hincrby(loser, 'point', 10)
+
+
+def total_up_points(room_id, winner, loser):
+    z_name = f'{room_id}:betting'
+    pair_list = r.zrange(name=z_name, start=0, end=-1, withscores=True)
+    for (v, s) in pair_list:
+        parsed_v = parse_bytes_into_str(v)
+        int_s = int(s)
+        if parsed_v == loser:
+            r.hincrby(winner, 'point', int_s)
+        else:
+            r.hincrby(loser, 'point', -int_s)
